1df37aae2225c86b53ca00b1c6f8ddcba624364e
jamaldonadoa <jadolfomaldonadoa@gmail.com>
2021-09-21 00:09:46 +0000

    ejercicios javascript

--- exercises/12-Your-first-if/app.js
+++ exercises/12-Your-first-if/app.js
@@ -1,3 +1,15 @@
 var total = prompt('How many km are left to go?');
 
 // Your code below:
+
+var mensaje
+
+if (total > 100) {
+    mensaje="We still have a bit of driving left to go"
+} else if (total > 50) {
+    mensaje = "We'll be there in 5 minutes"
+} else {
+    mensaje ="I'm parking. I'll see you right now"
+}
+
+return mensaje
