3aad76d3df367adbda02bf0b31728630f60556ca
Jesus Maldonado <jadolfomaldonadoa@gmail.com>
2021-09-25 12:32:29 -0300

    Todos los ejercicios

--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -1,7 +1,5 @@
 // Your code here:
 
-function bottles () {
-
     for (let i = 99; i > -1; i--) {
         if (i > 1 ) {
             console.log(i + " bottles of milk on the wall, " + i + " bottles of milk. Take one down and pass it around, " + (i-1) + " bottles of milk on the wall.")
@@ -11,6 +9,3 @@ function bottles () {
             console.log("No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.")
         } 
     }    
-}
-
-console.log(bottles())
--- exercises/25-Javascript-Objects/app.js
+++ exercises/25-Javascript-Objects/app.js
@@ -24,15 +24,26 @@ var family = {
 
 function addAllFamilyLuckyNumbers(anArray){
   var sumOfAllLuckyNumbers = 0; //sumOfAllLuckyNumbers is a number, the sum of all lucky numbers.
-
-  //To-Do: loop and add; consider nested loops
-  //Hint: use the anArray variable to get all of the lucky numbers
-  
+  var alllucky = [...person.lucky_numbers, ...person2.lucky_numbers, ...person3.lucky_numbers]
+        alllucky.forEach (function(numero){
+        sumOfAllLuckyNumbers += numero;
+    });
   return sumOfAllLuckyNumbers;
 }
 
 //Enter all your code here:
+person.lucky_numbers[3]=33
 
+ var person3 = {
+    name: "Jimmy",                
+    lastname: "Doe",
+    age: 13,                     
+    gender: "male",
+    lucky_numbers: [ 1, 2, 3, 4], 
+    significant_other: null   
+};
+
+family.members= [person, person2, person3]
 
 //Do not make changes below:
 console.log(addAllFamilyLuckyNumbers(family.members)); 
